3bf7438825471a57a660ece8eadfa6509d3a80ca
Evilmoose <ronnielopez@gmail.com>
2024-04-03 10:53:58 -0700

    top-level Madlib component and MadlibForm component snippets added

---
+++ src/Components/Madlib.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+// Madlib is the top-level component that manages stats of the madlib game
+const Madlib = () =>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Madlib;
---
+++ src/Components/MadlibForm.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+// Collects words from the user through a form.
+const MadlibForm = () => {
+    return (
+        <div>
+            {/* Form goes here*/}
+        </div>
+    )
+}
